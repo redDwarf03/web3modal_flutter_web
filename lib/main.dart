1f4e7871bc2fbbd753259c4797e264ecfb4db529
redDwarf03 <sylvain@archethic.net>
2024-08-19 23:21:46 +0200

    Add examples with getAccount, getToken, signMessage

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,65 @@ external JSWindow get window;
 extension type JSWindow(JSObject _) implements JSObject {
   external void openModal();
   external void closeModal();
+  external Account getAccount();
+  external int getChainId();
+  external JSPromise<Token> getToken(JSString address, int chainId);
+  external JSPromise<JSString> signMessage(
+      JSString message, JSString accountAddress);
+}
+
+@JS()
+extension type Account(JSObject _) implements JSObject {
+  external String? get address;
+  external String? get status;
+  external Chain? get chain;
+  external int? get chainId;
+  external Connector? get connector;
+  external bool? isConnecting;
+  external bool? isReconnecting;
+  external bool? isConnected;
+  external bool? isDisconnected;
+}
+
+@JS()
+extension type Connector(JSObject _) implements JSObject {
+  external bool? multiInjectedProviderDiscovery;
+  external bool? ssr;
+  external bool? syncConnectedChain;
+}
+
+@JS()
+extension type Chain(JSObject _) implements JSObject {
+  external int? id;
+  external String? name;
+}
+
+@JS()
+extension type Token(JSObject _) implements JSObject {
+  external String? address;
+  external int? decimals;
+  external String? name;
+  external String? symbol;
 }
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  void openModal() {
-    window.openModal();
-  }
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  void closeModal() {
-    window.closeModal();
-  }
+class _MyAppState extends State<MyApp> {
+  var chainId = 0;
+  Token? token;
+  Account? account;
+  String? signedMessage;
+  final tokenAddressToSearch = '0xCBBd3374090113732393DAE1433Bc14E5233d5d7';
+  final messageToSign = 'Hello World';
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +83,109 @@ class MyApp extends StatelessWidget {
                 },
                 child: const Text('Connect Wallet'),
               ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    signedMessage = null;
+                    account = getAccount();
+                    chainId = getChainId();
+                  });
+                },
+                child: const Text('Get Account info'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text('account address: ${account?.address ?? 'unknown'}'),
+              Text('account status:  ${account?.status ?? 'unknown'}'),
+              Text('account chain ID: ${account?.chainId ?? 'unknown'}'),
+              Text('Chain ID: $chainId'),
+              ElevatedButton(
+                onPressed: () async {
+                  token = await getToken(
+                      tokenAddressToSearch, account?.chainId ?? 0);
+                  setState(() {});
+                },
+                child: Text(
+                    'Get Token info ($tokenAddressToSearch / ${account?.chainId})'),
+              ),
+              if (token != null)
+                Column(
+                  children: [
+                    Text('token address: ${token?.address}'),
+                    Text('token name: ${token?.name}'),
+                    Text('token decimals: ${token?.decimals}'),
+                    Text('token symbol: ${token?.symbol}'),
+                  ],
+                ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  signedMessage = await signMessage(messageToSign);
+                  setState(() {});
+                },
+                child: Text('Personal sign ($messageToSign)'),
+              ),
+              if (signedMessage != null)
+                Column(
+                  children: [
+                    Text('message signed: $signedMessage'),
+                  ],
+                ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void openModal() {
+    window.openModal();
+  }
+
+  void closeModal() {
+    window.closeModal();
+  }
+
+  Account getAccount() {
+    return window.getAccount();
+  }
+
+  int getChainId() {
+    return window.getChainId();
+  }
+
+  Future<Token?> getToken(String address, int chainId) async {
+    try {
+      Token token = await window.getToken(address.toJS, chainId).toDart;
+      return token;
+    } catch (e) {
+      print("Error fetching token: $e");
+      return null;
+    }
+  }
+
+  Future<String?> signMessage(String message) async {
+    try {
+      final account = getAccount();
+      final accountAddress = account.address;
+
+      if (accountAddress == null || accountAddress.isEmpty) {
+        print("No valid account address found.");
+        return null;
+      }
+
+      final signedMessage =
+          await window.signMessage(message.toJS, accountAddress.toJS).toDart;
+      return signedMessage.toString();
+    } catch (e) {
+      print("Error sign message: $e");
+      return null;
+    }
   }
 }
--- web/main.js
+++ web/main.js
@@ -2,9 +2,9 @@ import { WagmiCore, Chains, Web3modal, Connectors } from "https://cdn.jsdelivr.n
 
 const { createWeb3Modal, defaultWagmiConfig } = Web3modal
 const { mainnet, sepolia } = Chains
-const { reconnect } = WagmiCore
+const { reconnect, getAccount, getChainId, getToken, signMessage } = WagmiCore
 
-const projectId = 'cad....'
+const projectId = '.....'
 
 const metadata = {
   name: 'Web3Modal',
@@ -28,10 +28,59 @@ const modal = createWeb3Modal({
 })
 
 // Expose functions to the global `window` object
-window.openModal = function() {
+window.openModal = function () {
   return modal.open()
 }
 
-window.closeModal = function() {
+window.closeModal = function () {
   return modal.close()
+}
+
+window.getAccount = function () {
+  const account = getAccount(config);
+  return account;
+}
+
+window.getChainId = function () {
+  const chainId = getChainId(config);
+  return chainId;
+}
+
+window.getToken = async function (address, chainId) {
+  if (!address) {
+    console.error("No token address provided");
+    return null;
+  }
+  try {
+    const token = await getToken(config, {
+      address: address,
+      chainId: chainId,
+    });
+    return token;
+  } catch (error) {
+    console.error("Error fetching token info:", error);
+    return null;
+  }
+}
+
+window.signMessage = async function (message, accountAddress) {
+  if (!message) {
+    console.error("No message provided");
+    return null;
+  }
+  if (!accountAddress || !/^0x[a-fA-F0-9]{40}$/.test(accountAddress)) {
+    console.error("Invalid account address provided");
+    return null;
+  }
+  try {
+    const signedMessage = await signMessage(config, {
+      account: accountAddress,
+      message: message,
+    });
+
+    return signedMessage;
+  } catch (error) {
+    console.error("Error message sign:", error);
+    return null;
+  }
 }
